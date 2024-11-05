4323f74974b6821d7c6d30f0c50fe87b8f3474c5
Ebadur Rahman <ebadurshohag@gmail.com>
2024-11-05 09:23:57 +0600

    initial commit

--- app.js
+++ app.js
@@ -1,17 +1,4 @@
-const http = require("http");
-
-const server = http.createServer((req, res) => {
-    // console.log(req);
-    if (req.url === "/") {
-        res.end("Welcome to home page...");
-    } else if (req.url === "/about") {
-        res.end("Here is our short history...");
-    } else {
-        res.end(`<h1>Oops</h1>
-            <p>We can't seem to find the page you are looking for</p>
-            <a href = "/">back to home</a>`);
-    }
-});
-
-server.listen(5000);
-console.log("server is running");
+const lodash = require("lodash");
+const items = [1, [2, [3, [4]]]];
+const newItems = lodash.flattenDeep(items);
+console.log(newItems);
